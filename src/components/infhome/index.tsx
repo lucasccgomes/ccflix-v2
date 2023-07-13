eb509714c690b387303482a2c9868c099acb2704
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-13 04:40:03 -0300

    feat(infhome):new component description in the home

---
+++ src/components/infhome/index.tsx
@@ -0,0 +1,42 @@
+export const InfHome = () => {
+  return (
+    <div
+      className="min-w-[112rem]
+    max-lg:min-w-[20rem]
+    mt-[14rem]
+    flex
+    flex-col
+    text-white
+    text-center  
+    items-center
+    "
+    >
+      <div
+        className="basis-1/2
+      p-0
+      z-[1]
+      w-full
+      text-center
+      "
+      >
+        <h1
+          className="text-[3rem]
+        max-lg:text-[2.5rem]
+        max-md:text-[2rem]
+        font-extrabold
+        "
+        >
+          Filmes, séries e muito mais, sem limites
+        </h1>
+        <p
+          className="text-[1.5rem]
+        font-medium
+        text-center
+        "
+        >
+          Assista onde quiser. Cancele quando quiser.
+        </p>
+      </div>
+    </div>
+  );
+};
