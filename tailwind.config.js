b6cdeba43a7bc51e562112441b6975aab2e1b1e7
Lucas de Oliveira Gomes <101642200+lucasccgomes@users.noreply.github.com>
2023-07-13 03:04:16 -0300

    feat(tailwind):update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "img/efect":"linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)",
+      }
+
+    },
     colors: {
+      "black/40":"rgb(0 0 0 / 40%)",
       "white":"#fff",
-      "bt-hover-red":"#C11119",
-      "bt-primary-red":"#E50914"
+      "bt/hover/red":"#C11119",
+      "bt/primary/red":"#E50914"
     },
   },
   plugins: [],
