b9797c1e466180742e7264c0054777e0a448c4b4
Lucasccgomes <lucasccgomes@protonmail.com>
2023-10-12 12:49:39 -0300

    fix(app):Removal of unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
 import { Aptv } from './components/aptv'
-import { Assine } from './components/assine'
 import BgHeader from './components/bgheader'
-import Button from './components/button'
 import { InfHome } from './components/infhome'
-import InInput from './components/input'
 import { Navbar } from './components/navbar'
 
 function App() {
@@ -14,7 +11,6 @@ function App() {
       <BgHeader>
         <Navbar />
         <InfHome />
-     
       </BgHeader>
         <Aptv />
     </>
